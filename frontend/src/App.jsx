172660240b933e0e61d57baaed9a1325e59dfd38
Ziad Hosny <ziadhosny007@gmail.com>
2023-01-26 16:37:27 +0200

    refactor code and add bootstrap and react router

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} />
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
