ae6964c24040310166aacd8179b50c002ccc31d6
ziad <ziad.hosny@bitspire.ca>
2023-09-05 21:46:26 +0300

    add private and admin Routes

---
+++ frontend/src/components/AdminRoute.jsx
@@ -0,0 +1,11 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+export const AdminRoute = () => {
+    const { userInfo } = useSelector((state) => state.auth);
+    return userInfo && userInfo.isAdmin ? (
+        <Outlet />
+    ) : (
+        <Navigate to='/login' replace />
+    );
+};
