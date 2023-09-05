2c376b22b493b77fb76f79bb1b6061cd6d0e9cf3
ziad <ziad.hosny@bitspire.ca>
2023-09-05 20:37:08 +0300

    add Custom Meta to project

---
+++ frontend/src/components/Meta.jsx
@@ -0,0 +1,17 @@
+import { Helmet } from 'react-helmet-async';
+
+export const Meta = ({ title, description, keywords }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name='description' content={description} />
+      <meta name='keyword' content={keywords} />
+    </Helmet>
+  );
+};
+
+Meta.defaultProps = {
+  title: 'Welcome To ProShop',
+  description: 'We sell the best products for cheap',
+  keywords: 'electronics, buy electronics, cheap electroincs',
+};
