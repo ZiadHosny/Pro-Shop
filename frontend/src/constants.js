fc8610afed4a86916df24710650d26e51a92b24f
ziad <ziad.hosny@bitspire.ca>
2023-09-10 15:37:07 +0300

    add proxy

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,5 +1,3 @@
-// export const BASE_URL = 'https://pro-shop-backend-z.vercel.app';
-// export const BASE_URL = 'http://localhost:5000';
 export const BASE_URL = '';
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
