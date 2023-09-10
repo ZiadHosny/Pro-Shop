47194f71ebfa40b6f57b06eb06ac11f693ad4082
ziad <ziad.hosny@bitspire.ca>
2023-09-10 22:59:47 +0300

    change vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,8 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     server: {
+        port: 3000,
         proxy: {
-            '/api': `https://pro-shop-backend-z.vercel.app`,
+            '/api': {
+                target: `https://pro-shop-backend-z.vercel.app`,
+                changeOrigin: true,
+                pathRewrite: { '^/api': '/' }
+            }
         }
     }
 })
