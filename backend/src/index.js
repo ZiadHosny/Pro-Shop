9a73ce8ed0240a0f20ac8264a877f5fb507801ed
ziad <ziad.hosny@bitspire.ca>
2023-09-10 15:15:46 +0300

    add proxy

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,8 +15,8 @@ connectDB()
 const app = express()
 
 app.use(cors());
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/products', productRoutes)
