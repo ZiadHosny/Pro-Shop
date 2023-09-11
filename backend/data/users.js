bb07c3c0cea91ca437706469f25ec060ca9912e8
ziad <ziad.hosny@bitspire.ca>
2023-09-11 23:54:06 +0300

    refactor code

---
+++ backend/data/users.js
@@ -0,0 +1,20 @@
+import bcrypt from 'bcryptjs';
+
+export const users = [
+    {
+        name: 'Admin User',
+        email: 'admin@email.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: true,
+    },
+    {
+        name: 'John Doe',
+        email: 'john@email.com',
+        password: bcrypt.hashSync('123456', 10),
+    },
+    {
+        name: 'Jane Doe',
+        email: 'jane@email.com',
+        password: bcrypt.hashSync('123456', 10),
+    },
+];
--- backend/index.js
+++ backend/index.js
@@ -31,16 +31,16 @@ app.get('/api/config/paypal', (req, res) =>
 
 if (process.env.NODE_ENV === 'production') {
     const __dirname = path.resolve();
-    console.log(path.resolve())
-    // app.use('/uploads', express.static('/var/data/uploads'));
+
+    app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
     app.use(express.static(path.join(__dirname, '/frontend/build')));
 
     app.get('*', (req, res) =>
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     );
 } else {
-    // const __dirname = path.resolve();
-    // app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+    const __dirname = path.resolve();
+    app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
     app.get('/', (req, res) => {
         res.send('API is running....');
     });
