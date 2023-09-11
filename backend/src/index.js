a86a32e3bdabb3a09127112a24417e8bdebaa1ca
ziad <ziad.hosny@bitspire.ca>
2023-09-11 23:10:48 +0300

    try deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from 'dotenv'
 import cors from 'cors'
+import path from 'path'
 import 'colors'
 import cookieParser from 'cookie-parser';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
@@ -28,21 +29,23 @@ app.get('/api/config/paypal', (req, res) =>
     res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
 
-// if (process.env.NODE_ENV === 'production') {
-//     const __dirname = path.resolve();
-//     app.use('/uploads', express.static('/var/data/uploads'));
-//     app.use(express.static(path.join(__dirname, '/frontend/build')));
+console.log(path.resolve())
 
-//     app.get('*', (req, res) =>
-//         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-//     );
-// } else {
-//     const __dirname = path.resolve();
-//     app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-//     app.get('/', (req, res) => {
-//         res.send('API is running....');
-//     });
-// }
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    // app.use('/uploads', express.static('/var/data/uploads'));
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    );
+} else {
+    // const __dirname = path.resolve();
+    // app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+    app.get('/', (req, res) => {
+        res.send('API is running....');
+    });
+}
 
 app.use(notFound)
 app.use(errorHandler)
