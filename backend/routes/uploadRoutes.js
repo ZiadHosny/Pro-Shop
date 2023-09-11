f18c4cc4113cf6d7216cfb1a5582550e269d3f5b
ziad <ziad.hosny@bitspire.ca>
2023-09-12 01:01:41 +0300

    try fix images

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -3,10 +3,11 @@ import express from 'express';
 import multer from 'multer';
 
 const router = express.Router();
+const __dirname = path.resolve();
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, 'uploads/');
+    cb(null, `${__dirname}/uploads/`);
   },
   filename(req, file, cb) {
     cb(
